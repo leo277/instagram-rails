14e43bcd3c5be557edea6de7b9656eaf3af0ce4d
leo277 <yongkai8@hotmail.com>
2018-03-20 13:44:41 +0800

    Add pic controller actions

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,37 @@
 class PicsController < ApplicationController
+
+	before_action :load_pic, only: [:show, :edit, :update]
+
 	def index
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	private
+	def load_pic
+		@pic = Pic.find(params[:id])
+	end
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
+	end
+
 end
